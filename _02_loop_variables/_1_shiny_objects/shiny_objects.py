747276072f7c2eca590ca61d98754db3100c7133
luke <luke.fun59@gmail.com>
2022-07-01 20:02:17 -0700

    cool

--- _02_loop_variables/_1_shiny_objects/shiny_objects.py
+++ _02_loop_variables/_1_shiny_objects/shiny_objects.py
@@ -1,7 +1,7 @@
 from tkinter import simpledialog, Tk
 from playsound import playsound
 
-can_play_sounds = False
+can_play_sounds = True
 
 
 def play_mister_zee():
@@ -15,7 +15,10 @@ def many_shiny_objects():
     # TODO 1) Call the method above to play Mister Zee
 
     # TODO 2) Ask the user how many shiny objects they want
+    bob=simpledialog.askinteger(title='shiny',prompt='how many shiny objects you want?')
 
+    for i in range (bob):
+        play_mister_zee()
     # TODO 3) Play the sound that many times
 
     pass
--- _02_loop_variables/_2_turtle_walk_of_fame/walk_of_fame.py
+++ _02_loop_variables/_2_turtle_walk_of_fame/walk_of_fame.py
@@ -1,14 +1,25 @@
 import turtle
 
 if __name__ == '__main__':
-    my_turtle = turtle.Turtle()
-    my_turtle.shape('turtle')
-    my_turtle.color('blue', 'green')
-    my_turtle.speed(100)
-
+    bill = turtle.Turtle()
+    bill.shape('turtle')
+    bill.color('blue', 'green')
+    bill.speed(0)
+    bill.penup()
     # TODO 1) Set the X position of the turtle so that it starts on the left.
-
+    bill.goto(-400,0)
+    bill.pendown()
     # TODO 2) Make the turtle draw a star shape. Hint: angle=144.
+    x=-450
+    for y in range(1000000000):
+        for i in range(6):
+            bill.right(144)
+            bill.forward(30)
+        bill.penup()
+        bill.goto(x,0)
+        x=x-50
+        bill.right(216)
+        bill.pendown()
 
     # TODO 3) Set the length of each line in the star to 30
 
@@ -16,6 +27,7 @@ if __name__ == '__main__':
     #       Make the turtle draw a line of stars like the image in
     #       this folder.
     #       Hint: The distance between stars is 50.
+
 
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     turtle.done()
